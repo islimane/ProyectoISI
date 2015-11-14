421e0a97b3ab7bc904eca9f31b01daf839ded275
Ismael Solanilla <i.solanilla3@gmail.com>
2015-11-14 13:48:05 +0100

    añado funcionalidad nuevo pagina usuario

--- Platform/PageUser.js
+++ Platform/PageUser.js
@@ -4,7 +4,10 @@ if (Meteor.isClient) {
 	Template.UserPage.helpers({
 		'infoUser':function(){
 			return Users.findOne({_id:this._id});
-		}
+		},
+    'ranking':function(){
+      return Users.find().fetch()
+    }
 	})
 }
 
