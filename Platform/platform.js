b8ca7163fba7370d078bd0595fea9686bc14ae3f
arubiopa <anggylotis@gmail.com>
2015-11-17 11:23:15 +0100

    añado modificaciones en nombres

--- Platform/platform.js
+++ Platform/platform.js
@@ -33,8 +33,8 @@ Router.route('/user/:_id', {
     }
 });
 Router.route('/comenzarPartida/:_id', {
-    template:"comenzarPartida",    
-    name:"ConfigPartida", 
+    template:"startGame",    
+    name:"configGame", 
     data: function(){
       var userID = this.params._id;
       return Users.findOne({_id:userID});
