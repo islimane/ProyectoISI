fefc2b189fc7f7475e981ae007500fd35abc0408
Ismael Solanilla <i.solanilla3@gmail.com>
2015-11-17 16:09:29 +0100

    pequeñas modificaciones

--- Platform/PageUser.js
+++ Platform/PageUser.js
@@ -3,9 +3,20 @@ if (Meteor.isClient) {
 		'infoUser':function(){
 			return Users.findOne({_id:this._id});
 		},
-    'ranking':function(){
-      return Users.find().fetch()
-    }
+		'myfriends':function(){
+			var idfriends = Users.findOne({_id:this._id}).friends
+			var arrfriends = []
+			for(i = 0; i < idfriends.length;i++){
+				var data = {
+					id:idfriends[i],
+					name : Users.findOne({_id:idfriends[i]}).user
+				}
+				arrfriends.push(data)
+			} 
+			console.log(arrfriends)
+			return arrfriends;
+		}
+
 	})
 }
 
