a7bbc95eb9869ceae4fb8799ce49fa2486f91d23
reysam93 <samuel.rey.escudero@gmail.com>
2015-11-18 12:54:08 +0100

    error raro con los helpers

--- Platform/home.js
+++ Platform/home.js
@@ -1,13 +1,18 @@
+
 if(Meteor.isCLient){
 	Template.home.helpers({
-		'otherPlayers': function(){
-			return null;
+		"otherPlayers": function(){
+			var players = Users.find().fetch();
+			return players.slice(0,15);
 		},
-		'waitingGames': function(){
-			return null;
+		"waitingGames": function(){
+			return Games.find({gameStart: false}).fetch();
 		},
-		'onGoingGames': function(){
-			return null;
+		"onGoingGames": function(){
+			return Games.find({gameStart: true}).fetch();
+		},
+		"prueba": function(){
+			return "TEXTO DE PRUEBA";
 		}
 	});
 }
