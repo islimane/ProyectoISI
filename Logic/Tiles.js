902b39819cec8bdd603b5b605fc1906502b8373f
j.peraltar <getajoel@gmail.com>
2015-11-20 21:13:10 +0100

    Fixed initTiles method and merged Tiles.js with predefTiles.js

--- Logic/Tiles.js
+++ Logic/Tiles.js
@@ -2,6 +2,154 @@
 //    TILES OBJECT     //
 /////////////////////////
 
+predefTiles = [
+    {
+        type: 0,
+        //         [ n,  nw,   w,  sw,   s,  se,   e,  ne,   c  ]
+        positions: ['f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'cl'],
+        total: 4
+    },
+    {
+        type: 1,
+        //         [ n,  nw,   w,  sw,   s,  se,   e,  ne,   c  ]
+        positions: ['f', 'f', 'f', 'f', 'r', 'f', 'f', 'f', 'cl'],
+        total: 2
+    },
+    {
+        type: 2,
+        //         [ n,    nw,   w,    sw,   s,    se,   e,    ne,   c  ]
+        positions: ['ci', 'b', 'ci', 'ci', 'ci', 'ci', 'ci', 'ci', 'ci'],
+        total: 1
+    },
+    {
+        type: 3,
+        //         [ n,    nw,   w,   sw,  s,   se,   e,   ne,   c  ]
+        positions: ['ci', 'ci', 'ci', 'f', 'f', 'f', 'ci', 'ci', 'ci'],
+        total: 3
+    },
+    {
+        type: 4,
+        //         [ n,   nw,   w,   sw,   s,  se,   e,   ne,   c  ]
+        positions: ['ci', 'b', 'ci', 'f', 'f', 'f', 'ci', 'ci', 'ci'],
+        total: 1
+    },
+    {
+        type: 5,
+        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
+        positions: ['ci', 'ci', 'ci', 'f', 'r', 'f', 'ci', 'ci', 'ci'],
+        total: 1
+    },
+    {
+        type: 6,
+        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
+        positions: ['ci', 'b', 'ci', 'f', 'r', 'f', 'ci', 'ci', 'ci'],
+        total: 2   
+    },
+    {
+        type: 7,
+        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
+        positions: ['ci', 'ci', 'ci', 'f', 'f', 'f', 'f', 'f', 'f'],
+        total: 3   
+    },
+    {
+        type: 8,
+        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
+        positions: ['ci', 'b', 'ci', 'f', 'f', 'f', 'f', 'f', 'f'],
+        total: 2   
+    },
+    {
+        type: 9,
+        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
+        positions: ['ci', 'ci', 'ci', 'f', 'r', 'f', 'r', 'f', 'f'],
+        total: 3   
+    },
+    {
+        type: 10,
+        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
+        positions: ['ci', 'b', 'ci', 'f', 'r', 'f', 'r', 'f', 'f'],
+        total: 2   
+    },
+    {
+        type: 11,
+        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
+        positions: ['f',  'f',  'ci', 'f', 'f', 'f', 'ci', 'f', 'ci'],
+        total: 1  
+    },
+    {
+        type: 12,
+        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
+        positions: ['f',  'f',  'ci', 'f', 'f', 'f', 'b', 'f', 'ci'],
+        total: 2  
+    },
+    {
+        type: 13,
+        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
+        positions: ['ci',  'f',  'ci', 'f', 'f', 'f', 'f', 'f', 'f'],
+        total: 2  
+    },
+    {
+        type: 14,
+        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
+        positions: ['ci',  'f',  'f', 'f', 'ci', 'f', 'f', 'f', 'f'],
+        total: 3  
+    },
+    {
+        type: 15,
+        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
+        positions: ['ci',  'f',  'f', 'f', 'f', 'f', 'f', 'f', 'f'],
+        total: 5  
+    },
+    {
+        type: 16,
+        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
+        positions: ['ci',  'f',  'r', 'f', 'r', 'f', 'f', 'f', 'f'],
+        total: 3 
+    },
+    {
+        type: 17,
+        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
+        positions: ['ci',  'f',  'f', 'f', 'r', 'f', 'r', 'f', 'f'],
+        total: 3 
+    },
+    {
+        type: 18,
+        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
+        positions: ['ci',  'f',  'r', 'f', 'r', 'f', 'r', 'f', 'x'],
+        total: 3 
+    },
+    {
+        type: 19,
+        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
+        positions: ['ci',  'f',  'r', 'f', 'f', 'f', 'r', 'f', 'r'],
+        total: 4 
+    },
+    {
+        type: 20,
+        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
+        positions: ['r',  'f',   'f', 'f', 'r', 'f', 'f', 'f',  'r'],
+        total: 8 
+    },
+    {
+        type: 21,
+        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
+        positions: ['f',  'f',  'r', 'f', 'r', 'f', 'f', 'f',  'f'],
+        total: 9 
+    },
+    {
+        type: 22,
+        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
+        positions: ['f',  'f',   'r', 'f', 'r', 'f', 'r', 'f',  'x'],
+        total: 4 
+    },
+    {
+        type: 23,
+        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
+        positions: ['r',  'f',   'r', 'f', 'r', 'f', 'r', 'f',  'x'],
+        total: 1 
+    },
+
+];
+
 Tiles = function() {
     this.queue = [];
     this.currentTile = null;
@@ -10,7 +158,7 @@ Tiles = function() {
 //Returns random int between maximum(excluded)
 //and minimum(included)
 function getRandomArbitrary (max, min) {
-    return Math.random() * (max - min) + min;
+    return Math.floor(Math.random() * (max - min) + min);
 }
 
 //Returns true if there are tiles left from
@@ -24,15 +172,15 @@ function filterByTotal (obj) {
 }
 
 Tiles.prototype.initTiles = function() {
-    while(queue.length < 72){
+    while(this.queue.length < 72){
         var remainingTiles = predefTiles.filter(filterByTotal);
         var Type = getRandomArbitrary(remainingTiles.length,0);
-        --predefTiles[Type].total;
-
+        --remainingTiles[Type].total;
         var Tile = {
-            type: predefTiles[Type].type,
-            positions: predefTiles[Type].positions
+            type: remainingTiles[Type].type,
+            positions: remainingTiles[Type].positions
         };
+        console.log(Tile.type);
         this.queue.push(Tile);
     }
 };
@@ -42,3 +190,6 @@ Tiles.prototype.popTile = function() {
         this.currentTile = this.queue.pop();
     }
 };
+
+var tiles = new Tiles();
+tiles.initTiles();
