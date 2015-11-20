0d9aba1a38eb27984345ee888a0ac8791d6fd68a
j.peraltar <getajoel@gmail.com>
2015-11-20 11:59:17 +0100

    Added initTiles method and modified popTile from the Tiles object

--- Logic/Tiles.js
+++ Logic/Tiles.js
@@ -7,14 +7,38 @@ Tiles = function() {
     this.currentTile = null;
 }
 
-Tiles.prototype.initTiles = function() {
-    // body...
+//Returns random int between maximum(excluded)
+//and minimum(included)
+function getRandomArbitrary (max, min) {
+    return Math.random() * (max - min) + min;
 }
+
+//Returns true if there are tiles left from
+//each type, false if the contrary
+function filterByTotal (obj) {
+    if(obj.total > 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+Tiles.prototype.initTiles = function() {
+    while(queue.length < 72){
+        var remainingTiles = predefTiles.filter(filterByTotal);
+        var Type = getRandomArbitrary(remainingTiles.length,0);
+        --predefTiles[Type].total;
+
+        var Tile = {
+            type: predefTiles[Type].type,
+            positions: predefTiles[Type].positions
+        };
+        this.queue.push(Tile);
+    }
+};
 
 Tiles.prototype.popTile = function() {
     if(this.queue.length > 0){
-        return this.queue.pop();
-    }else{
-        return null;
+        this.currentTile = this.queue.pop();
     }
 };
