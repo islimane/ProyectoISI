6ab08b52d555c781819c7c80d94c3f62c38897c4
jjmerchante <jj.merchante@gmail.com>
2015-11-20 20:03:23 +0100

    fixing somo details

--- Logic/Game.js
+++ Logic/Game.js
@@ -7,12 +7,13 @@
 Game = function(playersNames){
     this.players = new Players (playersNames);
     this.tiles = new Tiles();
+    this.tile.initTiles();
     this.board = new Board();
 }
 
 Game.prototype.startGame = function() {
-    while (tiles.length > 0){
+    while (tiles.queue.length > 0){
         Players.nextPlayer();
-        Tiles.nextTile();
+        Tiles.popTile();
     }
 };
