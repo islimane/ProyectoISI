bd7ccebf77b8e8e42b8f7902bde3ac44c178e6f7
Ismael Slimane Zubillaga <isma.bled777@gmail.com>
2015-11-21 15:41:05 +0100

    Fixed BoardSpec.js test error

--- tests/jasmine/server/integration/BoardSpec.js
+++ tests/jasmine/server/integration/BoardSpec.js
@@ -2,9 +2,9 @@ describe('Board', function() {
 
   it('should be a [HORIZONTAL_MAXSIZE]x[VERTICAL_MAXSIZE] array', function() {
   	var board = new Board();
-  	expect(board.length).toEqual(HORIZONTAL_MAXSIZE);
+  	expect(board.cells.length).toEqual(HORIZONTAL_MAXSIZE);
   	for(var i=0;i<HORIZONTAL_MAXSIZE;i++){
-        expect(board[i].length).toEqual(VERTICAL_MAXSIZE);
+        expect(board.cells[i].length).toEqual(VERTICAL_MAXSIZE);
     }
     var tile = new Tile(0, 1);
     var tileTurned = new Tile (0, 2);
