5e473fc6a01790502e35d58a984f53f96f5f7c65
Ismael Slimane Zubillaga <isma.bled777@gmail.com>
2015-11-21 15:53:31 +0100

    Added test to BoardSpec.js testing tiles insertion on the board

--- tests/jasmine/server/integration/BoardSpec.js
+++ tests/jasmine/server/integration/BoardSpec.js
@@ -1,14 +1,18 @@
 describe('Board', function() {
 
-  it('should be a [HORIZONTAL_MAXSIZE]x[VERTICAL_MAXSIZE] array', function() {
-  	var board = new Board();
-  	expect(board.cells.length).toEqual(HORIZONTAL_MAXSIZE);
-  	for(var i=0;i<HORIZONTAL_MAXSIZE;i++){
-        expect(board.cells[i].length).toEqual(VERTICAL_MAXSIZE);
-    }
-    var tile = new Tile(0, 1);
-    var tileTurned = new Tile (0, 2);
-    tile.turnTile();
-    expect(tile).toEqual(tileTurned);
-  });
+	it('should be a [HORIZONTAL_MAXSIZE]x[VERTICAL_MAXSIZE] array', function() {
+		var board = new Board();
+		expect(board.cells.length).toEqual(HORIZONTAL_MAXSIZE);
+		for(var i=0;i<HORIZONTAL_MAXSIZE;i++){
+		    expect(board.cells[i].length).toEqual(VERTICAL_MAXSIZE);
+		}
+	});
+
+	it('should insert a tile on the board with type=19 and orientation=0', function() {
+		var board = new Board();
+		var t = new Tile(19, 0);
+		board.cells[49][49].tile = t;
+		expect(board.cells[49][49].tile.type).toEqual(19);
+		expect(board.cells[49][49].tile.orientation).toEqual(0);
+	});
 });
