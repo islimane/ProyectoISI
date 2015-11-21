85807e7b221491dfd6de0fec3ca36b9d99d7d379
Ismael Slimane Zubillaga <isma.bled777@gmail.com>
2015-11-21 18:06:44 +0100

    Fixed typo

--- Logic/Board.js
+++ Logic/Board.js
@@ -1,229 +1,12 @@
-predefTiles = [
-    {
-        type: 0,
-        //         [ n,  nw,   w,  sw,   s,  se,   e,  ne,   c  ]
-        positions: ['f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'cl'],
-        total: 4
-    },
-    {
-        type: 1,
-        //         [ n,  nw,   w,  sw,   s,  se,   e,  ne,   c  ]
-        positions: ['f', 'f', 'f', 'f', 'r', 'f', 'f', 'f', 'cl'],
-        total: 2
-    },
-    {
-        type: 2,
-        //         [ n,    nw,   w,    sw,   s,    se,   e,    ne,   c  ]
-        positions: ['ci', 'b', 'ci', 'ci', 'ci', 'ci', 'ci', 'ci', 'ci'],
-        total: 1
-    },
-    {
-        type: 3,
-        //         [ n,    nw,   w,   sw,  s,   se,   e,   ne,   c  ]
-        positions: ['ci', 'ci', 'ci', 'f', 'f', 'f', 'ci', 'ci', 'ci'],
-        total: 3
-    },
-    {
-        type: 4,
-        //         [ n,   nw,   w,   sw,   s,  se,   e,   ne,   c  ]
-        positions: ['ci', 'b', 'ci', 'f', 'f', 'f', 'ci', 'ci', 'ci'],
-        total: 1
-    },
-    {
-        type: 5,
-        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
-        positions: ['ci', 'ci', 'ci', 'f', 'r', 'f', 'ci', 'ci', 'ci'],
-        total: 1
-    },
-    {
-        type: 6,
-        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
-        positions: ['ci', 'b', 'ci', 'f', 'r', 'f', 'ci', 'ci', 'ci'],
-        total: 2   
-    },
-    {
-        type: 7,
-        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
-        positions: ['ci', 'ci', 'ci', 'f', 'f', 'f', 'f', 'f', 'f'],
-        total: 3   
-    },
-    {
-        type: 8,
-        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
-        positions: ['ci', 'b', 'ci', 'f', 'f', 'f', 'f', 'f', 'f'],
-        total: 2   
-    },
-    {
-        type: 9,
-        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
-        positions: ['ci', 'ci', 'ci', 'f', 'r', 'f', 'r', 'f', 'f'],
-        total: 3   
-    },
-    {
-        type: 10,
-        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
-        positions: ['ci', 'b', 'ci', 'f', 'r', 'f', 'r', 'f', 'f'],
-        total: 2   
-    },
-    {
-        type: 11,
-        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
-        positions: ['f',  'f',  'ci', 'f', 'f', 'f', 'ci', 'f', 'ci'],
-        total: 1  
-    },
-    {
-        type: 12,
-        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
-        positions: ['f',  'f',  'ci', 'f', 'f', 'f', 'b', 'f', 'ci'],
-        total: 2  
-    },
-    {
-        type: 13,
-        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
-        positions: ['ci',  'f',  'ci', 'f', 'f', 'f', 'f', 'f', 'f'],
-        total: 2  
-    },
-    {
-        type: 14,
-        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
-        positions: ['ci',  'f',  'f', 'f', 'ci', 'f', 'f', 'f', 'f'],
-        total: 3  
-    },
-    {
-        type: 15,
-        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
-        positions: ['ci',  'f',  'f', 'f', 'f', 'f', 'f', 'f', 'f'],
-        total: 5  
-    },
-    {
-        type: 16,
-        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
-        positions: ['ci',  'f',  'r', 'f', 'r', 'f', 'f', 'f', 'f'],
-        total: 3 
-    },
-    {
-        type: 17,
-        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
-        positions: ['ci',  'f',  'f', 'f', 'r', 'f', 'r', 'f', 'f'],
-        total: 3 
-    },
-    {
-        type: 18,
-        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
-        positions: ['ci',  'f',  'r', 'f', 'r', 'f', 'r', 'f', 'x'],
-        total: 3 
-    },
-    {
-        type: 19,
-        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
-        positions: ['ci',  'f',  'r', 'f', 'f', 'f', 'r', 'f', 'r'],
-        total: 4 
-    },
-    {
-        type: 20,
-        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
-        positions: ['r',  'f',   'f', 'f', 'r', 'f', 'f', 'f',  'r'],
-        total: 8 
-    },
-    {
-        type: 21,
-        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
-        positions: ['f',  'f',  'r', 'f', 'r', 'f', 'f', 'f',  'f'],
-        total: 9 
-    },
-    {
-        type: 22,
-        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
-        positions: ['f',  'f',   'r', 'f', 'r', 'f', 'r', 'f',  'x'],
-        total: 4 
-    },
-    {
-        type: 23,
-        //         [ n,    nw,   w,   sw,   s,  se,   e,   ne,   c  ]
-        positions: ['r',  'f',   'r', 'f', 'r', 'f', 'r', 'f',  'x'],
-        total: 1 
-    },
-
-];
-
-////////////////////////
-//    TILE OBJECT     //
-////////////////////////
-
-// args:
-// orientation: 0, 1, 2, 3 (clockwise)
-Tile = function (type, orientation){
-    this.type = type;
-    var tmpTile = predefTiles[type].positions;
-    this.positions = {
-        get n  () { return predefTiles[type].positions[(0+orientation*2)%8]; },
-        get nw () { return predefTiles[type].positions[(1+orientation*2)%8]; },
-        get w  () { return predefTiles[type].positions[(2+orientation*2)%8]; },
-        get sw () { return predefTiles[type].positions[(3+orientation*2)%8]; },
-        get s  () { return predefTiles[type].positions[(4+orientation*2)%8]; },
-        get se () { return predefTiles[type].positions[(5+orientation*2)%8]; },
-        get e  () { return predefTiles[type].positions[(6+orientation*2)%8]; },
-        get ne () { return predefTiles[type].positions[(7+orientation*2)%8]; },
-        get c  () { return predefTiles[type].positions[8]; }
-    }
-    this.orientation = orientation;
-}
-
-// Turns 90 degrees clockwise
-Tile.prototype.turnTile = function(){
-    this.orientation = (this.orientation + 1) % 4;
-}
-
-/////////////////////////
-//    TILES OBJECT     //
-/////////////////////////
-
-Tiles = function() {
-    this.queue = [];
-    this.currentTile = null;
-}
-
-//Returns random int between maximum(excluded)
-//and minimum(included)
-function getRandomArbitrary (max, min) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
-
-//Returns true if there are tiles left from
-//each type, false if the contrary
-function filterByTotal (obj) {
-    if(obj.total > 0){
-        return true;
-    }else{
-        return false;
-    }
-}
-
-Tiles.prototype.initTiles = function() {
-    var startingTiles = JSON.parse(JSON.stringify(predefTiles));
-    while(this.queue.length < 72){
-        var remainingTiles = startingTiles.filter(filterByTotal);
-        var Type = getRandomArbitrary(remainingTiles.length,0);
-        --remainingTiles[Type].total;
-        var tile = new Tile(remainingTiles[Type].type, 0);
-        console.log(tile.type);
-        this.queue.push(tile);
-    }
-};
-
-Tiles.prototype.popTile = function() {
-    if(this.queue.length > 0){
-        this.currentTile = this.queue.pop();
-    }
-};
-
 /////////////////////////////
 //      CELL OBJECT        //
 /////////////////////////////
 
 // cell constructor, the cell is initialized without tile
-Cell = function(){
+Cell = function(x, y){
     this.tile = null;
+    this.x = x;
+    this.y = y;
 }
 
 /////////////////////////////
@@ -238,13 +21,13 @@ Board = function(){
     // this field is an array of cells
     // in wich we may have a posibility
     // to put a tile if it matches
-    this.avalibleCells = [];
+    this.availableCells = [];
 }
 
 // this function returns an array
 // of cells in wich we can put
 // the given tile
-Board.prototype.getAvalibleCell = function(tile){
+Board.prototype.getAvailableCell = function(tile){
     //body
 }
 
@@ -261,7 +44,7 @@ function initializeBoard(){
     for(var i=0;i<HORIZONTAL_MAXSIZE;i++){
         var column = [];
         for(var j=0;j<VERTICAL_MAXSIZE;j++){
-            column.push(new Cell());
+            column.push(new Cell(i, j));
         }
         cells.push(column);
         column = [];
@@ -271,3 +54,8 @@ function initializeBoard(){
     cells[49][49].tile = t;
     return cells;
 }
+
+/*var b = new Board();
+console.log(b.cells[49][49]);
+b.insertTile();
+console.log(b.cells[49][50]);*/
