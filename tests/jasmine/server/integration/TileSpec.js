e489faaf184e9e83f3374c942925d126704de267
Ismael Slimane Zubillaga <isma.bled777@gmail.com>
2015-11-21 17:15:02 +0100

    Tested a tile instantiation

--- tests/jasmine/server/integration/TileSpec.js
+++ tests/jasmine/server/integration/TileSpec.js
@@ -1,4 +1,10 @@
 describe('Tile', function() {
+	it('should instantiate a tile object', function() {
+		var tile = new Tile(19, 0);
+		expect(tile.type).toEqual(19);
+		expect(tile.orientation).toEqual(0);
+	});
+
 
   it('should turn the Tile', function() {
     var tile = new Tile(0, 1);
