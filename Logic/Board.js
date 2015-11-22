e3e8a70129458fe87cc810efb4c7d4e11b81423f
Ismael Slimane Zubillaga <isma.bled777@gmail.com>
2015-11-22 15:50:48 +0100

    Added getMatchingCells(tile), this function returns an array of cells in which the given tile fits

--- Logic/Board.js
+++ Logic/Board.js
@@ -19,27 +19,42 @@ VERTICAL_MAXSIZE = 100;
 Board = function(){
     this.cells = initializeBoard();
     // this field is an array of cells
-    // in wich we may have a posibility
+    // in which we may have a posibility
     // to put a tile if it matches
-    this.availableCells = [];
-}
-
-// this function returns an array
-// of cells in wich we can put
-// the given tile
-Board.prototype.getMatchingbleCells = function(tile){
-    //body
+    this.availableCells = getAvailableCells(this);
 }
 
 // This function inserts a tile on the
 // given coordinates with a given orientation
-// coordinates: [xx,yy] --> e.g [4,52]
+// coor: [xx,yy] --> e.g [4,52]
 Board.prototype.insertTile = function(tileType, orientation, coor){
     var t = new Tile(tileType, orientation);
     this.cells[coor[0]][coor[1]].tile = t;
+    // Update availableCells
+    this.availableCells = getAvailableCells(this);
 }
 
-function initializeBoard(){
+// this function returns an array
+// of cells in which, the given tile,
+// fits
+Board.prototype.getMatchingCells = function(tile){
+    var matchingCells = [];
+    var that = this;
+    this.availableCells.forEach(function(elem, index){
+        var coor = [elem.x, elem.y];
+        if(itFits(that, tile, coor)){
+            if(!isInArray(elem, matchingCells))
+                matchingCells.push(elem);
+        }
+    });
+    return matchingCells;
+}
+
+var isInArray = function(value, array){
+  return array.indexOf(value) > -1;
+}
+
+var initializeBoard = function(){
     var cells = [];
     for(var i=0;i<HORIZONTAL_MAXSIZE;i++){
         var column = [];
@@ -56,7 +71,7 @@ function initializeBoard(){
 }
 
 // this this function returns an array of cells
-// in wich we may have a posibility
+// in which we may have a posibility
 // to put a tile if it matches
 var getAvailableCells = function(board){
     var availableCells = [];
@@ -115,9 +130,103 @@ var checkCell = function(cell, haveTile, emptyCell){
     return null;
 }
 
+// this function returns true if a given tile,
+// with the current orientation,
+// can fit in a cell on the given board by
+// checking the coordinates
+// coor: [xx,yy] --> e.g [4,52]
+var itFits = function(board, tile, coor){
+    var fits = false;
+    // this array contains the cells with which
+    // we are going to check if the tile fits
+    // testableCells = [northCell, eastCell, southCell, westCell]
+    var testableCells = getTestableCells(board, coor);
+    // console.log("testableCells:");
+    // console.log(testableCells);
+    // Now we walk the array checking if the tile
+    // fits or not
+    testableCells.forEach(function(elem, index){
+        // the elem is a cell object
+        if(elem.tile){
+            // this array contains the two zones we have to compare
+            // zones[0] -> zone of the current tile
+            // zone[1] -> zone of the board tile
+            var zones = getZones(tile, elem, index);
+            if(zones[0]==zones[1]){
+                fits = true;
+            }else{
+                fits = false;
+                return false;
+            }
+        }
+    });
+    return fits;
+}
+
+
+// this function returns an array containing
+// the cells with which we have to check if 
+// a tile fits
+// testableCells = [northCell, eastCell, southCell, westCell]
+var getTestableCells = function(board, coor){
+    var testableCells = [];
+
+    var northCell = board.cells[coor[0]][coor[1]-1];
+    testableCells.push(northCell);
+    var eastCell = board.cells[coor[0]+1][coor[1]];
+    testableCells.push(eastCell);
+    var southCell = board.cells[coor[0]][coor[1]+1];
+    testableCells.push(southCell);
+    var westCell = board.cells[coor[0]-1][coor[1]];
+    testableCells.push(westCell);
+
+    return testableCells;
+}
+
+// this function returns an array containing
+// the two zones we have to compare
+var getZones = function(tile, elem, index){
+    // testableCells = [northCell, eastCell, southCell, westCell]
+    // for index=0 -> we have to compare tile.positions.n->elem.tile.positions.s
+    // for index=1 -> we have to compare tile.positions.e->elem.tile.positions.w
+    // ...
+    switch (index) {
+      case 0:
+        var zone1 = tile.positions.n;
+        var zone2 = elem.tile.positions.s;
+        break;
+      case 1:
+        var zone1 = tile.positions.e;
+        var zone2 = elem.tile.positions.w;
+        break;
+      case 2:
+        var zone1 = tile.positions.s;
+        var zone2 = elem.tile.positions.n;
+        break;
+      default:
+        var zone1 = tile.positions.w;
+        var zone2 = elem.tile.positions.e;
+        break;
+    }
+    return [zone1, zone2];
+}
+
 /*var b = new Board();
 b.insertTile(19,0,[49,49]);
 b.insertTile(0,0,[49,50]);
 b.insertTile(20,0,[50,50]);
+b.insertTile(0,0,[51,50]);
+b.insertTile(15,0,[51,49]);
 
-console.log(getAvailableCells(b));*/
+console.log("availableCells:");
+console.log(b.availableCells);
+
+var t = new Tile(21, 0);
+
+console.log("matchingCells:");
+console.log(b.getMatchingCells(t));
+*/
+
+
+
+
