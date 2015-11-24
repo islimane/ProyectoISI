d51f0f4e9ea56c9489159e18c30a96a875424b30
LauraArteaga <larteaga1990@gmail.com>
2015-11-24 20:06:43 +0100

    Añadidas sesiones y cambio de plantilla

--- UI/InterfazUsuario.js
+++ UI/InterfazUsuario.js
@@ -4,6 +4,10 @@ if (Meteor.isClient) {
   TileList.Tile = new Array();
   var numTile = 72;
   var tileID;
+
+  Session.set('showPlaceTile', false);
+  Session.set('showRotateTile', false);
+  Session.set('showFollowers', false);
   
   var iterator;
   for(iterator=1; iterator<=numTile; iterator++)
@@ -15,27 +19,47 @@ if (Meteor.isClient) {
 	TileList.Tile[iterator].TablePos = undefined;
 	TileList.Tile[iterator].Follower = undefined;
   };
+  
   Template.game.generateTile = function(){
 
   	var idRandom = Math.random()*71 + 1;
   	return idRandom;
-
+  }, 
+  
+  Template.game.endOfTurn = function(){
+	Session.set('showPlaceTile', false);
+	Session.set('showRotateTile', false);
+	Session.set('showFollowers', false);
+	//Pasar info a logica
+	
   }, 
 
   Template.game.lookForTileSprite = function (id) {
-		var iterator;
-		var found = false;
-		for(iterator=1; iterator<=numTile; iterator++)
+	var iterator;
+	var found = false;
+	for(iterator=1; iterator<=numTile; iterator++)
+	{
+		if(TileList.Tile[iterator].Id == id)
 		{
-			if(TileList.Tile[iterator].Id == id)
-			{
-				break;
-			}
-		};
-		return TileList.Tile[iterator].Sprite;
+			break;
+		}
+	};
+	return TileList.Tile[iterator].Sprite;
   },
-	
+
   Template.game.helpers({
+    pickTile: function () {
+		return Session.get('showPickTile');
+	},
+	placeTile: function () {
+		return Session.get('showPlaceTile');
+	},
+	rotateTile: function () {
+		return Session.get('showRotateTile');
+	},
+	followers: function () {
+		return Session.get('showFollowers');
+	}
 	
   });
 
@@ -51,9 +75,11 @@ if (Meteor.isClient) {
 		//PrintActiveBox
 		//BlockingActiveBox
 		//PrintSprite
+		Session.set('showPlaceTile', true);
+		
     },
     'click button#PlaceTile': function () {
-		return null;
+		Session.set('showRotateTile', true);
     },
     'click button#RotateTile': function () {
 		var pos = TileList.Tile[tileID].RotationPos;
@@ -67,28 +93,29 @@ if (Meteor.isClient) {
 		console.log(pos);
     },
     'click button#Finish': function () {
-		return null;
+		Session.set('showFollowers', true);
     },
     'click #Followers .Thief': function () {
 		TileList.Tile[tileID].Follower = "Thief";
 		console.log(TileList.Tile[tileID].Follower);
-		//Fin de turno
+		Template.game.endOfTurn();
     },
     'click #Followers .Knigth': function () {
 		TileList.Tile[tileID].Follower = "Knigth";
 		console.log(TileList.Tile[tileID].Follower);
-		//Fin de turno
+		Template.game.endOfTurn();
     },
     'click #Followers .Monk': function () {
 		TileList.Tile[tileID].Follower = "Monk";
 		console.log(TileList.Tile[tileID].Follower);
-		//Fin de turno
+		Template.game.endOfTurn();
     },
     'click #Followers .Farmer': function () {
 		TileList.Tile[tileID].Follower = "Farmer";
 		console.log(TileList.Tile[tileID].Follower);
-		//Fin de turno
+		Template.game.endOfTurn();
     }
+    
   });
 }
 
