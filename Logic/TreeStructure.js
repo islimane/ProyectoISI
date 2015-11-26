4fd172e634c6bd51cb1f50793fcc2ef09a7e14e2
jjmerchante <jj.merchante@gmail.com>
2015-11-27 00:55:51 +0100

    Adding function for merge 2 parts of road or city or field

--- Logic/TreeStructure.js
+++ Logic/TreeStructure.js
@@ -5,8 +5,6 @@
 
 
 // type: 'r', 'f', 'ci'
-// firstNodeCoords: {x:1, y:2}
-// firstNodeChildrenCoords: [{x:2, y:2}, {x:1, y:3}, ...]
 MainNode = function(type){
 	this.type = type;
 	this.firstNode = undefined;
@@ -15,13 +13,15 @@ MainNode = function(type){
 
 // coord: {x:1, y:2}
 // childrenCoords: [{x:2, y:2}, {x:1, y:3}, ...]
+// Check first if exists the node in the tree. If exists,
+// call this function to indicate that is placed
 MainNode.prototype.placeNode = function(coord, childrenCoords) {
     var childrenCoords = childrenCoords || [];
 
 	if (this.firstNode == undefined){
-		this.firstNode = new Node(coord, childrenCoords, true);
+		this.firstNode = new Node(coord, null, childrenCoords, true);
 	} else {
-        childrenCoords = this._delDuplicateCoords(childrenCoords);
+        childrenCoords = this._getNotDupCoords(childrenCoords);
 		var node = this.findNode(coord);
 		if (node)
             node.placed = true;
@@ -30,16 +30,15 @@ MainNode.prototype.placeNode = function(coord, childrenCoords) {
 }
 
 
+// Returns frue or false if coord is in the tree
 // coord: {x:1, y:2}
-MainNode.prototype.findNode = function(coord){
-    var output = undefined;
-    if (this.firstNode == undefined){
-        output = undefined;
-    } else {
-        output = this.firstNode.findInChildren(coord);
-    }
-    return output;
+MainNode.prototype.existsNode = function(coord){
+    if (findNode(coord))
+        return true;
+    else
+        return false;
 }
+
 
 // Returns the number of tiles not placed
 MainNode.prototype.getNumNotPlaced = function(){
@@ -59,9 +58,23 @@ MainNode.prototype.getNumPlaced = function(){
 }
 
 
+// Merge two trees
+// Call this when you're going to place a coord that is in two trees.
+// coord is the common point in the two trees
+MainNode.prototype.mergeWith = function(remoteTree, coord){
+    remoteTree._setFirstNode(coord);
+    var nodeDel = this.findNode(coord);
+    var parent = nodeDel.parent;
+    var delIndex = parent.children.indexOf(nodeDel);
+    parent.children.splice(delIndex, 1);
+    parent.children.push(remoteTree.firstNode);
+    // TODO: deleteDupNodes();
+
+}
+
 
 //private methods: _
-MainNode.prototype._delDuplicateCoords = function(coords){
+MainNode.prototype._getNotDupCoords = function(coords){
     var outCoords = [];
     for (var i = 0; i < coords.length; i++) {
         if (this.findNode(coords[i]) == undefined){
@@ -71,11 +84,47 @@ MainNode.prototype._delDuplicateCoords = function(coords){
     return outCoords;
 }
 
+// Change the first node to coord's node
+MainNode.prototype._setFirstNode = function(coord){
+    var firstNode = this.findNode(coord);
+    if (!firstNode)
+        return;
+    var aux = firstNode;
+    var prev = null;
+    var parent = null;
+    var delIndex = 0;
+    while (aux){
+        parent = aux.parent;
+        aux.parent = prev;
+        if (prev){
+            delIndex = aux.children.indexOf(prev);
+            aux.children.splice(delIndex, 1);
+        }
+        if (parent)
+            aux.children.push(parent);
+        prev = aux;
+        aux = parent;
+    }
+    this.firstNode = firstNode;
+}
 
+// coord: {x:1, y:2}
+MainNode.prototype.findNode = function(coord){
+    var output = undefined;
+    if (this.firstNode == undefined){
+        output = undefined;
+    } else {
+        output = this.firstNode.findInChildren(coord);
+    }
+    return output;
+}
 
 // For debug issues
 MainNode.prototype.printTree = function(){
-    this.firstNode.printTree(1);
+    if (this.firstNode)
+        this.firstNode.printTree(1);
+    else
+        console.log("Empty tree");
 }
 
 
@@ -89,12 +138,13 @@ MainNode.prototype.printTree = function(){
 // coord: {x:1, y:2}
 // childrenCoords: [{x:2, y:2}, {x:1, y:3}, ...]
 // placed: true or false
-Node = function(coord, childrenCoords, placed){
+Node = function(coord, parent, childrenCoords, placed){
     var childrenCoords = childrenCoords || [];
     var placed = placed || false;
     this.coord = coord;
 	this.placed = placed;
-	this.nodes = [];
+    this.parent = parent || null;
+	this.children = [];
     this.setChildren(childrenCoords);
 }
 
@@ -104,12 +154,12 @@ Node = function(coord, childrenCoords, placed){
 Node.prototype.findInChildren = function(coord) {
 	var nodeOut = undefined;
     if (coord){
-    	for (var i = 0; i < this.nodes.length; i++){
-    		if (this.nodes[i].coord.x == coord.x && this.nodes[i].coord.y == coord.y){
-    			nodeOut = this.nodes[i];
+    	for (var i = 0; i < this.children.length; i++){
+    		if (this.children[i].coord.x == coord.x && this.children[i].coord.y == coord.y){
+    			nodeOut = this.children[i];
     			break;
     		} else {
-    			nodeOut = this.nodes[i].findInChildren(coord);
+    			nodeOut = this.children[i].findInChildren(coord);
     			if (nodeOut != undefined)
     				break;
     		}
@@ -122,7 +172,7 @@ Node.prototype.findInChildren = function(coord) {
 Node.prototype.setChildren = function(childrenCoords){
     var childrenCoords = childrenCoords || [];
 	for (var i = 0; i < childrenCoords.length; i++){
-		this.nodes.push(new Node(childrenCoords[i]));
+		this.children.push(new Node(childrenCoords[i], this));
 	}
 }
 
@@ -132,8 +182,8 @@ Node.prototype.numChildrenNotPlaced = function (){
     var num = 0;
     if (!this.placed)
         num++;
-    for (var i=0; i<this.nodes.length; i++){
-        num += this.nodes[i].numChildrenNotPlaced();
+    for (var i=0; i<this.children.length; i++){
+        num += this.children[i].numChildrenNotPlaced();
     }
     return num;
 }
@@ -144,8 +194,8 @@ Node.prototype.numChildrenPlaced = function (){
     var num = 0;
     if (this.placed)
         num++;
-    for (var i=0; i<this.nodes.length; i++){
-        num += this.nodes[i].numChildrenPlaced();
+    for (var i=0; i<this.children.length; i++){
+        num += this.children[i].numChildrenPlaced();
     }
     return num;
 }
@@ -155,8 +205,8 @@ Node.prototype.numChildrenPlaced = function (){
 Node.prototype.printTree = function(indent){
     console.log(Array(2 * indent).join(" ") + "- coord(" + 
         this.coord.x + "," + this.coord.y +")Placed:" + this.placed);
-    for (var i=0; i<this.nodes.length; i++){
-        this.nodes[i].printTree(indent + 1);
+    for (var i=0; i<this.children.length; i++){
+        this.children[i].printTree(indent + 1);
     }
 }
 
@@ -171,17 +221,38 @@ coord3 = {x: 3, y: 3};
 coord4 = {x: 4, y: 4};
 coord5 = {x: 5, y: 5};
 coord6 = {x: 6, y: 6};
+coord7 = {x: 7, y: 7};
+coord8 = {x: 8, y: 8};
+coord9 = {x: 9, y: 9};
 
-console.log("Creating mainNode");
-var fieldTree = new MainNode('f');
+console.log("--- Creating mainNode 1 ---");
+var fieldTree1 = new MainNode('f');
 console.log("Adding c1");
-fieldTree.placeNode(coord1, [coord2, coord3]);
+fieldTree1.placeNode(coord1, [coord2, coord3]);
 console.log("Adding c2");
-fieldTree.placeNode(coord2, [coord4, coord2]);
+fieldTree1.placeNode(coord2, [coord4, coord3]);
 console.log("Adding c4");
-fieldTree.placeNode(coord4);
+fieldTree1.placeNode(coord4);
 console.log("END");
 
-console.log("not placed: " + fieldTree.getNumNotPlaced());
-console.log("placed: " + fieldTree.getNumPlaced());
-fieldTree.printTree()*/
+console.log("not placed: " + fieldTree1.getNumNotPlaced());
+console.log("placed: " + fieldTree1.getNumPlaced());
+fieldTree1.printTree();
+
+
+console.log("--- Creating mainNode 2 ---");
+var fieldTree2 = new MainNode('f');
+console.log("Adding c1");
+fieldTree2.placeNode(coord5, [coord4, coord6]);
+console.log("Adding c2");
+fieldTree2.placeNode(coord6, [coord7, coord4]);
+console.log("END");
+
+console.log("not placed: " + fieldTree2.getNumNotPlaced());
+console.log("placed: " + fieldTree2.getNumPlaced());
+fieldTree2.printTree();
+
+console.log("--- Merging the trees ---");
+fieldTree1.mergeWith(fieldTree2, coord4);
+console.log("Merged!");
+fieldTree1.printTree();*/
