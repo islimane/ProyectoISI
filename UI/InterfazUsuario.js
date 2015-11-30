af54cd847bc088a13b31bae9b2298e5ff61caeee
LauraArteaga <larteaga1990@gmail.com>
2015-11-30 18:37:50 +0100

    Actualizados ficheros

--- UI/InterfazUsuario.js
+++ UI/InterfazUsuario.js
@@ -1,85 +1,122 @@
 if (Meteor.isClient) {
 
-	var fichasActvas = [];
-	var fichasValidas = [];
-	var jugadores = [];
+  var TileList = new Object();
+  TileList.Tile = new Array();
+  var numTile = 72;
+  var tileID;
 
-	function jugador(posicion, puntos, nombre){
-		this.pos=posicion;
-		this.puntos=puntos;
-		this.nombre=nombre;
-	}
+  Session.set('showPlaceTile', false);
+  Session.set('showRotateTile', false);
+  Session.set('showFollowers', false);
+  
+  var iterator;
+  for(iterator=1; iterator<=numTile; iterator++)
+  {
+	TileList.Tile[iterator] = new Object();
+	TileList.Tile[iterator].Id = iterator;
+	TileList.Tile[iterator].Sprite = iterator;
+	TileList.Tile[iterator].RotationPos = 1;
+	TileList.Tile[iterator].TablePos = undefined;
+	TileList.Tile[iterator].Follower = undefined;
+  };
+  
+  Template.game.generateTile = function(){
 
-	function ficha(num, coordx, coordy, rot, token) {
-	    this.num = num;
-	    this.coordx = coordx;
-	    this.coordy = coordy;
-	    this.rot = rot;
-	    this.token=token;
-	}
-Template.meterJugadores.events({
-	'submit form': function(event){
-	    event.preventDefault();
-	    var pos = event.target.pos.value;
-	    var puntos = event.target.puntos.value;
-	    var nombre = event.target.nombre.value;
+  	var idRandom = Math.random()*71 + 1;
+  	return idRandom;
+  }, 
+  
+  Template.game.endOfTurn = function(){
+	Session.set('showPlaceTile', false);
+	Session.set('showRotateTile', false);
+	Session.set('showFollowers', false);
+	//Pasar info a logica
+	
+  }, 
 
-	    jugadores[pos] = new jugador(pos,puntos,nombre);
-	}
-});
-Template.meterfichaActiva.events({
-	'submit form': function(event){
-	    event.preventDefault();
-	    var num = event.target.num.value;
-	    var coordx = event.target.coordx.value;
-	    var coordy = event.target.coordy.value;
-	    var rot = event.target.rot.value;
-	    var token= event.target.token.value;
-	    var ficha= new ficha(num,coordx,coordy,rot,token);
-	    fichasActivas.push(ficha);
-	}
-
-});
-
-Template.meterfichaValida.events({
-	'submit form': function(event){
-	    event.preventDefault();
-	    var num = event.target.num.value;
-	    var coordx = event.target.coordx.value;
-	    var coordy = event.target.coordy.value;
-	    var rot = event.target.rot.value;
-	    var token= event.target.token.value;
-	    var ficha= new ficha(num,coordx,coordy,rot,token);
-	    fichasValidas.push(ficha);
-	}
-
-});
-
-Template.puntosAjugadores.events({
-	'submit form': function(event){
-		event.preventDefault();
-		var j1= event.target.jugador1.value;
-		var j2= event.target.jugador2.value;
-		var j3= event.target.jugador3.value;
-		var j4= event.target.jugador4.value;
-		if (j1){
-			jugadores[0].puntos = jugadores[0].puntos +j1;
+  Template.game.lookForTileSprite = function (id) {
+	var iterator;
+	var found = false;
+	for(iterator=1; iterator<=numTile; iterator++)
+	{
+		if(TileList.Tile[iterator].Id == id)
+		{
+			break;
 		}
-		if (j2){
-			jugadores[1].puntos = jugadores[1].puntos +j2;
-		}
-		if (j3){
-			jugadores[2].puntos = jugadores[2].puntos +j3;
-		}
-		if (j4){
-			jugadores[3].puntos = jugadores[3].puntos +j4;
-		}
+	};
+	return TileList.Tile[iterator].Sprite;
+  },
 
+  Template.game.helpers({
+    pickTile: function () {
+		return Session.get('showPickTile');
+	},
+	placeTile: function () {
+		return Session.get('showPlaceTile');
+	},
+	rotateTile: function () {
+		return Session.get('showRotateTile');
+	},
+	followers: function () {
+		return Session.get('showFollowers');
 	}
-})
+	
+  });
 
-
-
+  Template.game.events({
+	  
+    'click button#PickTile': function () {
+		//tileID = RequestTile
+		//RequestActiveBox
+		var idRandom = Template.game.generateTile();
+		var sprite = Template.game.lookForTileSprite(parseInt(idRandom));
+		tileID = sprite;
+		console.log(sprite);
+		//PrintActiveBox
+		//BlockingActiveBox
+		//PrintSprite
+		Session.set('showPlaceTile', true);
+		
+    },
+    'click button#PlaceTile': function () {
+		Session.set('showRotateTile', true);
+    },
+    'click button#RotateTile': function () {
+		var pos = TileList.Tile[tileID].RotationPos;
+		console.log("Inicio: " + pos)
+		pos += 1;
+		if(pos > 4)
+		{
+			pos = 1;
+		}
+		TileList.Tile[tileID].RotationPos = pos;
+		console.log(pos);
+    },
+    'click button#Finish': function () {
+		Session.set('showFollowers', true);
+    },
+    'click #Followers .Thief': function () {
+		TileList.Tile[tileID].Follower = "Thief";
+		console.log(TileList.Tile[tileID].Follower);
+		Template.game.endOfTurn();
+    },
+    'click #Followers .Knigth': function () {
+		TileList.Tile[tileID].Follower = "Knigth";
+		console.log(TileList.Tile[tileID].Follower);
+		Template.game.endOfTurn();
+    },
+    'click #Followers .Monk': function () {
+		TileList.Tile[tileID].Follower = "Monk";
+		console.log(TileList.Tile[tileID].Follower);
+		Template.game.endOfTurn();
+    },
+    'click #Followers .Farmer': function () {
+		TileList.Tile[tileID].Follower = "Farmer";
+		console.log(TileList.Tile[tileID].Follower);
+		Template.game.endOfTurn();
+    }
+    
+  });
 }
 
 if (Meteor.isServer) {
