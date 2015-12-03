cb4f26c6f624a1d677bca3887781e4724316a628
Kaloyan Todorov <k.todorov@outlook.com>
2015-12-03 17:45:34 +0100

    Implemented all interaction methods with UI and its description in ui_interaction.txt

--- Logic/Msgs.js
+++ Logic/Msgs.js
@@ -1,47 +1,144 @@
-/*
 // This a collection to interact with UI, both ( UI and IA, server and client)
-// have acces to the collection and an event will take place if a modification
-// is realiced.
+// by some methods (called by client) to check the collection stored in the server.
 
 Msgs = new Mongo.Collection("msgs") ;
 
-msg {
-	idGame = "" ;
+// Server Methods about the Msgs collection.
+
+// Method to create the entry in Msgs shared between IA and UI
+initMsg = function (idGame) {
+	var msg = new Msg (idGame) ;		// Msg declare at the bottom
+	return Msgs.insert(msg) ;
+}
+
+// Fills the Games msg with the new turns information ( Player, Tile, positions..)
+newTurnMsg = function (Game, Player, Tile) {
+	var msg = Msgs.findOne({idGame: Game});
+	if (!msg) {
+		return null ;
+	}
+	msg.idPlayer = Player ;
+	msg.idTile = Tile ;
+}
+// Sets the plausible coords for the current tile.
+setCoords = function (Game , rot0, rot1, rot2, rot3) {
+	var msg = Msgs.findOne({idGame: Game});
+	if (!msg) {
+		return null ;
+	}
+	msg.coords.rot0 = rot0 ;
+	msg.coords.rot1 = rot1 ;
+	msg.coords.rot2 = rot2 ;
+	msg.coords.rot3 = rot3 ;
+}
+// Returns the info from UI
+getBackInfo = function (Game) {
+	var msg = Msgs.findOne({idGame: Game});
+	if (!msg) {
+		return null ;
+	}
+}
+
+// New info once checked backInfo from UI.
+setUpdatedInfo = function (Game , coords , score) {
+	var msg = Msgs.findOne({idGame: Game});
+	if (!msg) {
+		return null ;
+	}
+	msg.updatedInfo.coord = coords ;
+	msg.updatedInfo.score = score ;
+}
+// Removes the Msg entry for the game
+removeMsg = function (Game) {
+	Msgs.remove({idGame: Game})
+}
+
+
+//==> Client Methods to allow access to the collection from the client.
+
+Meteor.methods({
+
+	// Returns the current Player
+	getPlayer : function (Game) {
+		var msg = Msgs.findOne({idGame: Game});
+		if (!msg) {
+			return null ;
+		}
+		return msg.idPlayer ;
+	},
+
+	getTile : function (Game) {
+		var msg = Msgs.findOne({idGame: Game});
+		if (!msg) {
+			return null ;
+		}
+		return msg.idTile ;
+	},
+
+	// Returns the arry of the 4 rotations coords 
+	getCoords : function(Game){
+		var msg = Msgs.findOne({idGame: Game});
+		if (!msg) {
+			return null ;
+		}
+		return msg.coords ;
+	} ,
+	// Returns the updated info for UI, the new players score and the dummies
+	// coords to be removed.
+	getUpdatedInfo : function (Game) {
+		var msg = Msgs.findOne({idGame: Game});
+		if (!msg) {
+			return null ;
+		}
+		return msg.updatedInfo ;
+	},
+	// Set the positions when the tile is placed on the board and its rotation
+	setPos : function (Game, x, y, rot) {
+		var msg = Msgs.findOne({idGame: Game});
+		if (!msg) {
+			return null ;
+		}
+		msg.backInfo.x = x ;
+		msg.backInfo.y = y ;
+		msg.backInfo.rotation = rot ;
+	},
+	// Sets the position when dummy is placed in the tile.
+	setDummy : function (Game, arry) {
+		var msg = Msgs.findOne({idGame: Game});
+		if (!msg) {
+			return null ;
+		}
+		msg.backInfo.dummy = arry ;
+	},
+});
+
+
+
+// Fields of each collection entry
+Msg = function (idGame) {
+	this.idGame = idGame ;
 	idPlayer = "" ;		
-	idTile = "" ;		// Tile 0-23
+	idTile = -1 ;		// Tile type 0-23
 	
-	rotations = {
-		rot0: info[] ;
-		rot1: info[] ;
-		rot2: info[] ;
-		rot3: info[] ;
+	coords = {
+		rot0 : [] ,  	// arrays of {
+		rot1 : [] ,	// 		coord [x,y]
+		rot2 : [] ,	// 		dummy positions [ n,  nw,   w,  sw,   s,  se,   e,  ne,   c  ]
+		rot3 : [] ,
 	}
 
-	backInfo ;
-	playerUpdate ;
-	
-}
-// Info to be filled by IA
-info {
-	cordX ;
-	cordY ;
-	dummies: [];	// [ n,  nw,   w,  sw,   s,  se,   e,  ne,   c  ]
-	 		// de 0 a 9 true y false donde vaya el dummy
-}
-// Info to be filled by UI
-backInfo {
-	coordX;
-	coordY;
-	dummies: [] ; 	// [ n,  nw,   w,  sw,   s,  se,   e,  ne,   c  ]
-	 		// de 0 a 9 true y false donde vaya el dummy
-	rotation ;	// choosen tiles rotation 0-3
-}
+	// Info from UI
+	backInfo = {
+		x ,		// where the tile is placed on the board
+		y ,
+		dummy : [] , 	// [ n,  nw,   w,  sw,   s,  se,   e,  ne,   c  ]
+	 			// de 0 a 9 true y false donde vaya el dummy
+		rotation ,	// choosen tiles rotation 0-3
+	}
 
-
-playerUpdate {
-	coord ; 	// Coord of removed dummies
-	incScore = 0;
+	// Checked info for UI
+	updatedInfo = {
+		coord  : []  , 	// removed dummies coords 
+		score : 0 , 	// new score
+	}
 }
-*/	
-
-
