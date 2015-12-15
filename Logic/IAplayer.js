853f49d4086e809f6c9241723ae5297e1de97d21
j.peraltar <getajoel@gmail.com>
2015-12-15 16:25:42 +0100

    Added structure for IA players and their differents difficulty modes

---
+++ Logic/IAplayer.js
@@ -0,0 +1,40 @@
+/////////////////////////////
+//    IA PLAYER OBJECT     //
+/////////////////////////////
+
+// IA Player inherits from Player class
+// IAplayer is an abstract class
+IAplayer = function () {
+    Player.call(this, 0);
+
+    if (this.constructor === IAplayer) {
+        throw new Error("Can't instantiate abstract class");
+    }
+}
+
+IAplayer.prototype.playTile = function() {
+    throw new Error("Abstract method");
+};
+
+// IAplayer class extends Player
+IAplayer.prototype = Object.create(Player.prototype);
+IAplayer.prototype.constructor = IAplayer;
+
+
+/////////////////////////////////
+//    IA PLAYER FIRST MODE     //
+/////////////////////////////////
+
+// Constructor of the first IA Player mode
+// Inherits from IAplayer abstract class
+FirstMode = function () {
+    IAplayer.call(this);
+}
+
+// FirstMode class extends IAplayer
+FirstMode.prototype = Object.create(IAplayer.prototype);
+FirstMode.prototype.constructor = FirstMode;
+
+FirstMode.prototype.playTile = function (tile) {
+    // Yet to be implemented
+}
