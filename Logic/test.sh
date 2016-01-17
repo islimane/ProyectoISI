bbc88c071ae3f20aa453927ce3a315d94300a316
jjmerchante <jj.merchante@gmail.com>
2016-01-17 17:09:51 +0100

    Added a file for testing

---
+++ Logic/test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+fileName="/tmp/test-$(date +%Hh%Mm%Ss)-$RANDOM"
+echo "New file created for testing:" $fileName
+
+touch $fileName
+
+file[0]=TreeStructure_v2.js
+file[1]=TreesCollection.js
+file[2]=Tiles.js
+file[3]=Players.js
+file[4]=Games.js
+file[5]=Game.js
+file[6]=Dummy.js
+file[7]=Board.js
+file[8]=Msgs.js
+file[9]=PlayersIA.js
+file[10]=pruebas.js
+
+for f in ${file[*]};
+do
+    if [ -r $f ]; then
+	echo "adding" $f
+	cat $f >> $fileName
+	echo -e "\n\n\n\n\n" >> $fileName
+	echo "//==================================================================" >> $fileName
+	echo -e "\n\n\n\n\n" >> $fileName
+    fi
+done
+
+echo "_____BEGIN______"
+nodejs "$fileName"
+echo "______END_______"
+
+#rm $fileName
