c845951773d64a7ae3a78ee6bb055c408e3922e4
Kaloyan Todorov <k.todorov@outlook.com>
2016-01-18 09:37:29 +0100

    Msgs and IA player continued

--- Logic/PlayersIA.js
+++ Logic/PlayersIA.js
@@ -15,6 +15,14 @@ IAplayer = function () {
     }
 }
 
+/*
+    This function must return:
+    {
+        coord: [x, y]       where the tile is placed
+        dummy : Dummy       the dummy placed, if it, else null.       
+    }
+ */
+
 IAplayer.prototype.playTile = function() {
     throw new Error("Abstract method");
 };
@@ -42,16 +50,21 @@ FirstMode.prototype.constructor = FirstMode;
 // just assigns the tile to the first possition it finds
 // (If for a given orientation there are no possible positions
 // it tries with a new orientation of the tile)
-FirstMode.prototype.playTile = function (tile, board) {
-    var cells = [];
-    while (cells.length == 0) {
-        cells = board.getMatchingCells(tile);
-        if (cells.length == 0) {
-            tile.turnTile();
+FirstMode.prototype.playTile = function (game) {
+    var cells = game.board.getDummyPositions(game.tiles.currentTile);
+    for ( var i = 0 ; i < cells.length ; i++) {
+        if (cells[i].length != 0) {
+            game.board.insertTile(game.tiles.currentTile , cells[i][0].cell);
         }
+        game.board.tiles.currentTile.turnTile() ;
+    }
+    
+    var data = {
+        coord : cells[i][0].cell,
+        dummy : null 
     }
 
-    board.insertTile(tile.type, tile.orientation, cells[0]);
+    return data ;
 }
 
 
@@ -60,7 +73,7 @@ FirstMode.prototype.playTile = function (tile, board) {
 /////////////////////////////////
 
 /*
-    If has dummies check the longest available zone and inserts the tile with the dummy
+    If has dummies check the longest available zone and inserts the tile with the dummy.
     If has no dummies , inserts the tile to a zone with one of his dummies.
 */
 
@@ -72,6 +85,50 @@ SecondMode = function () {
 SecondMode.prototype = Object.create(IAplayer.prototype);
 SecondMode.prototype.constructor = SecondMode;
 
-SecondMode.prototype.playTile = function (tile, board) {
-    // TODO
+// game its the current game.
+// Asumes that he is the current player.
+SecondMode.prototype.playTile = function (game) {
+
+    var me = game.players.currentPlayer ;
+    var coords = game.board.getDummyPositions(game.tiles.currentTile) ;
+
+    if ( me.getDummy ){
+
+    }
+
+
+}
+
+var playWithDummy = function() {
+
+
+}
+
+var zoneTypes = function(tile) {
+    var c = false ;
+    var r = false ;
+    var f = false ;
+    var zones = new Array() ;
+    for (var i = 0 ; i < tile.tmpTile.length ; i++){
+        switch (tile.tmpTile[i]){
+            case 'ci':
+                if ( c == false ) {
+                    c = true ;
+                    zones.push(tile.tmpTile[i]) ;
+                }
+                break ;
+            case 'r':
+                if ( r == false) {
+                    r = true ;
+                    zones.push(tile.tmpTile[i]) ;
+                }
+                break ;
+            case 'f' :
+                if ( f == false) {
+                    f = true ;
+                    zones.push(tile.tmpTile[i]) ;
+                }
+        }
+    }
+    return zones ;
 }
